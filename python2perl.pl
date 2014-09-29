0f73caf91d11a34c4130166d6619cf16f2743c36
Mackenzie Baran <macka.baran@gmail.com>
2014-09-29 12:45:39 +1000

    started subset 1

--- python2perl.pl
+++ python2perl.pl
@@ -5,25 +5,37 @@
 
 while ($line = <>) {
     if ($line =~ /^#!/ && $. == 1) {
-        
-        # translate #! line
-        
+        # translate #! line... subs 0
         print "#!/usr/bin/perl -w\n";
-    } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
-
-        # Blank & comment lines can be passed unchanged
-
-        print $line;
-    } elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
     
+    } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+        # Blank & comment lines can be passed unchanged... subs 0
+        print $line;
+
+    } elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
         # Python's print print a new-line character by default
         # so we need to add it explicitly to the Perl print statement
-    
+        # ... subs 0
         print "print \"$1\\n\";\n";
+        
+    } elsif ($line =~ /^\s*print\s*([^\"]*)/){
+        # printing variables ... subs 1
+        print "print \"\$",($1),"\\n\";\n";
+
+    } elsif ($line =~ /^\s*(.*)\s*=\s*(.*)/){
+        #deal with $ variables ...subs 1
+        $sub = $2;
+        if ($sub =~ /(\s*\S*\s*[\(\)\+\-\*\/]\s*)+/){
+            while ($sub =~ m/(\s*\S*\s*[\+\-\*\/\%]+\s*)*/g){
+                print "-->",$1,"<--\n";
+            }
+        }else{
+            print "\$$1= $2;\n";
+        }
+        
     } else {
-    
         #Lines we can't translate are turned into comments
-    
         print "#$line\n";
-}
+        
+    }
 }
