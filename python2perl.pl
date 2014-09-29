a4606d38e6e62c35ac0400a0749906a0e3415e2c
Mackenzie Baran <macka.baran@gmail.com>
2014-09-29 10:56:39 +1000

    starting point

---
+++ python2perl.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+#  python2perl.pl
+#  Created by Mackenzie Baran on 27/09/2014.
+
+while ($line = <>) {
+    if ($line =~ /^#!/ && $. == 1) {
+        
+        # translate #! line
+        
+        print "#!/usr/bin/perl -w\n";
+    } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+
+        # Blank & comment lines can be passed unchanged
+
+        print $line;
+    } elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
+    
+        # Python's print print a new-line character by default
+        # so we need to add it explicitly to the Perl print statement
+    
+        print "print \"$1\\n\";\n";
+    } else {
+    
+        #Lines we can't translate are turned into comments
+    
+        print "#$line\n";
+}
+}
