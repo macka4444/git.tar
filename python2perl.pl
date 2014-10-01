740999d1a631f82244e485ef965a2b3c8b6ff933
Mackenzie Baran <macka.baran@gmail.com>
2014-10-01 17:01:16 +1000

    further subset 1 debugging

--- python2perl.pl
+++ python2perl.pl
@@ -22,36 +22,67 @@ while ($line = <>) {
         # printing variables ... subs 1
         $temp =$1;
         chomp $temp;
-        if ($temp =~ /(\s*\S*\s*[\(\)\+\-\*\/]\s*)+/){
-            print "print ";
-            while ($temp =~ /(\s*\S*\s*[\(\)\+\-\*\/]*\s*)/g){
-                print "$1";
+        print "print ";
+        
+        while ($temp =~ m/\s*(\S*)\s*([\,\;\(\)\+\-\*\/]*)/g){
+            $numvar = $1;
+            $operation = $2;
+            if ($numvar =~ m/[^\S][0-9]+\s*/){
+                print "$numvar $operation ";
+            }elsif ($numvar =~ /[\"\"]+/){
+                print "\"$numvar \", ";
+            }elsif ($numvar =~ /\s*[a-zA-Z0-9]+\s*/g){
+                print "\$$numvar $operation ";
             }
-            print ", \"\\n\";\n";
-        }else{
-            print "print \"\$",($temp),"\\n\";\n";
+            
         }
+        print ", \"\\n\";\n";
+        
+        
+        
+        
+        #if ($temp =~ /(\s*\S*\s*[\(\)\+\-\*\/]\s*)+/){
+        #    #case where rhs of eqn deals with arithmetic operators
+        #    print "print ";
+        #    while ($temp =~ /(\s*\S*\s*[\(\)\+\-\*\/]*\s*)/g){
+        #        print "$1";
+        #    }
+        #    print ", \"\\n\";\n";
+        #}else{
+        #    #case with no operators
+        #    print "print \"\$",($temp),"\\n\";\n";
+        #}
+    } elsif ($line =~ /\s*if\s*(.*):\s*(.*)/){
+        #single line if statements ...subs2
+        $cond = $1;
+        $arg = $2;
+        
+        print "if \($cond\){\n\t$arg;\n\}\n";
+        
     } elsif ($line =~ /^\s*(.*)\s*=\s*(.*)/){
         #deal with $ variables ...subs 1
         $lhs=$1;
         $rhs = $2;
-        if ($rhs =~ /(\s*[^a-zA-Z][0-9]+\s*[\+\-\*\/\%]*)+/){
-            print "\$$lhs= $rhs;\n";
-        }elsif ($rhs =~ /(\s*\S*\s*[\(\)\+\-\*\/]\s*)+/){
+        if ($rhs =~ /(\s*\S*\s*[\(\)\+\-\*\/]\s*)+/){
             print "\$",$lhs,' = ';
-            while ($rhs =~ m/(\s*\S*\s*[\+\-\*\/\%]*\s*)/g){
-                $temp = $1;
-                chomp $temp;
-                if ($temp =~ /[^\s]/){
+            while ($rhs =~ m/(\s*(\S*)\s*[\+\-\*\/\%]*\s*)/g){
+                $temp1 = $1;
+                chomp $temp1;
+                $temp2 =$2;
+                if ($temp2 =~ /[^\s0-9]/){
                     print "\$";
                 }
-                print $temp;
+                print $temp1;
             }
             print ";\n";
+            
         }else{
             print "\$$1= $2;\n";
+            
         }
-        
+    
+
+    
     } else {
         #Lines we can't translate are turned into comments
         print "#$line\n";
